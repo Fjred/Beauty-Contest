c3f2ee3df17695cc3bc466796f4830c427c10177
Fjred <karolis.aukstakojis@gmail.com>
2025-10-20 16:50:28 +0300

    Button Placement
    Fixed button placement

--- Assets/ButtonGrid.cs
+++ Assets/ButtonGrid.cs
@@ -4,20 +4,28 @@ using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class ButtonGrid : MonoBehaviour
 {
     [SerializeField] private GameObject buttonPrefab;
-    [SerializeField] private int totalButtons = 100;
+    [SerializeField] private int totalButtons = 101;
 
     private List<Button> buttons = new List<Button>();
     private bool generated = false;
 
     private int multiplier=10;
 
+    [SerializeField] int startingX;
+    [SerializeField] int startingY;
+
+
     // ✅ Call this when you want to spawn the buttons
     public void GenerateButtons()
     {
         if (generated) return; // prevent double spawning
+
+        int numX = 8;
+        int numY = 0;
 
         for (int i = 0; i < totalButtons; i++)
         {
@@ -26,12 +34,27 @@ public class ButtonGrid : MonoBehaviour
             int x;
             int y;
             
-            x = -125 + (i - 1) * 25;
-            y = (x-1) / 9 * 25;
+            if(numX == 9)
+            {
+                numX = 0;
+                numY += 1;
+            }
+            else
+            {
+                numX++;
+            }
+   
+            x = startingX + numX * 35;
+            y = startingY - numY * 35;
 
             Debug.Log(x);
             Debug.Log(y);
-            btnObj.transform.position = new Vector2(x, y);
+            RectTransform rect = btnObj.GetComponent<RectTransform>();
+            rect.anchorMin = new Vector2(0.5f, 0.5f);
+            rect.anchorMax = new Vector2(0.5f, 0.5f);
+            rect.pivot = new Vector2(0.5f, 0.5f);
+
+            rect.anchoredPosition = new Vector2(x, y);
 
             int number = i;
             btnObj.GetComponentInChildren<TextMeshProUGUI>().text = number.ToString();
